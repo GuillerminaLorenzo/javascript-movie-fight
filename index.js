3fc4b59a0e86214b37fc9068fcc7ffb81cef6a91
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-01-25 14:56:36 +0000

    Fetching data with axios by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetchData = async () => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'd24ffbe3',
-            s: 'avengers'
+            i: 'tt0848228'
         }
     });
     console.log(response.data);
