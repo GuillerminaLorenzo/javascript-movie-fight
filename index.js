5867053ef646e51de2e0fbb2cecf6a65bfaa5e22
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-02-08 16:49:36 +0000

    Comparing movies

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ createAutoComplete({
     root: document.querySelector('#left-autocomplete'),
     onOptionSelect(movie) {
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#left-summary'));
+        onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
     }
 });
 
@@ -39,12 +39,14 @@ createAutoComplete({
     root: document.querySelector('#right-autocomplete'),
     onOptionSelect(movie) {
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#right-summary'));
+        onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
     }
 });
 
+let leftMovie;
+let rightMovie;
 
-const onMovieSelect = async (movie, summaryElement) => {
+const onMovieSelect = async (movie, summaryElement, side) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'd24ffbe3',
@@ -53,6 +55,20 @@ const onMovieSelect = async (movie, summaryElement) => {
     });
 
     summaryElement.innerHTML = movieTemplate(response.data);
+
+    if (side === 'left') {
+        leftMovie = response.data;
+    } else {
+        rightMovie = response.data;
+    }
+
+    if (leftMovie && rightMovie) {
+        runComparison();
+    }
+};
+
+const runComparison = () => {
+    console.log('ok')
 };
 
 const movieTemplate = movieDetail => {
